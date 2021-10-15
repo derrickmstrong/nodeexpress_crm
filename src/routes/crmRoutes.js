5f75d05dc147a0b7b83cce45a595347661982c11
Derrick Strong <derrickmstrong@gmail.com>
2021-10-15 07:04:11 -0500

    Added middleware

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,21 +1,26 @@
 const routes = app => {
   app
     .route('/contacts')
-    .get((req, res) => {
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from: ${req.originalUrl}`)
+      console.log(`Request type: ${req.method}`)
+      next()
+    }, (req, res, next ) => {
       res.send('GET request successful');
     })
     .post((req, res) => {
       res.send('POST request successful');
     });
 
-    app
-    .route('/contacts/:contactID')
+  app
+    .route('/contacts/:id')
     .put((req, res) => {
-        res.send('PUT request successful')
+      res.send('PUT request successful');
     })
     .delete((req, res) => {
-        res.send('DELETE request successful')
-    })
+      res.send('DELETE request successful');
+    });
 };
 
 export default routes;
