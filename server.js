9ac0296b1ff308990ce58c38160c6e5dd08df1ee
Derrick Strong <derrickmstrong@gmail.com>
2021-10-15 07:06:29 -0500

    Connected Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express(),
   PORT = 4000;
+
+// connect mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+  useNewUrlParser: true,
+  useUnifiedToplogy: true,
+});
 
 routes(app);
 
