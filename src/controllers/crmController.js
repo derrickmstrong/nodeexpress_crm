4ac213cebb05af3d4af0a724ef59465ca27e74ca
Derrick Strong <derrickmstrong@gmail.com>
2021-10-15 13:54:55 -0500

    Created get(all) endpoint

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -3,10 +3,19 @@ import { ContactSchema } from '../models/crmModel';
 
 const Contact = mongoose.model('Conact', ContactSchema);
 
+// Create
 export const addNewContact = (req, res) => {
   let newContact = new Contact(req.body);
 
   newContact.save((err, contact) => {
+    if (err) return res.send(err);
+    res.json(contact);
+  });
+};
+
+// Read
+export const getContacts = (req, res) => {
+    Contact.find({}, (err, contact) => {
     if (err) return res.send(err);
     res.json(contact);
   });
